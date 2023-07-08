16fd1f2ee5c42a08db9e551b4a251f3c6c17a833
Peter McKasty <pmck25@yahoo.com>
2023-07-08 12:28:17 -0400

    added get routes to category.js to get all categories and get one category by ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,28 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    // Find all categories and include associated Products
+    const categories = await Category.findAll({ include: Product });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    // Find one category by its `id` value and include associated Products
+    const category = await Category.findByPk(req.params.id, { include: Product });
+    if (!category) {
+      res.status(404).json({ message: 'No category found with this id.' });
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
