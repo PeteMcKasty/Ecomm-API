68d838f37b2a9b8f5653dad6d6dbaf9a78d6edf0
Peter McKasty <pmck25@yahoo.com>
2023-07-08 15:55:45 -0400

    added delete route to remove product tag in tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,12 +37,43 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    // Update a tag's name by its `id` value
+    const updatedTag = await Tag.update(
+      { tag_name: req.body.tag_name },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!updatedTag[0]) {
+      res.status(404).json({ message: 'No tag found with this id.' });
+      return;
+    }
+    res.status(200).json({ message: 'Tag updated successfully.' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    // Delete one tag by its `id` value
+    const tag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with this id.' });
+      return;
+    }
+    res.status(200).json({ message: 'Tag deleted successfully.' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
