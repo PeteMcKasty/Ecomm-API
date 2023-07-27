5d3517591d6356a7f998da4020fd3cebb9a2bda3
Peter McKasty <pmck25@yahoo.com>
2023-07-26 20:55:36 -0400

    debugged server.js to require sequelize and sync model functions

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./models');
-// import sequelize connection
+const sequelize = require('./config/connection');
+require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
